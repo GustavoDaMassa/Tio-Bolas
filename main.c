431f016422bd7f1b058832a7702fa28dd23d56ab
Anderson Barros Rigonatto <111210996+AndersonRigonatto@users.noreply.github.com>
2023-02-15 14:20:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 #include "arquivos.h"
 #include "pedido.h"
 #include "produto.h"
+#include "menu.h"
 #include <unistd.h>
 #include <stdlib.h>
 
 int main(){
-    int num = -1, c=0;
-    char senha[15]={"030201TioBolas?"};
-    char tentativa[100]={0};
+    int num = -1;
 
     /*
     Função que registra uma função para ser executada ao fim do programa
@@ -19,6 +18,7 @@ int main(){
     abrir_arquivos();
 
     load_cardapio();
+    load_clientes();
 
     h = (Head *) malloc(sizeof(Head));
     if (!h) {
@@ -27,6 +27,7 @@ int main(){
     }
 
     while(1) {
+        num = -1;
         printf("\nBem vinde a sorveteria Tio Bolas!\n\n");
         printf("Digite um numero para escolher qual interface deseja acessar:\n");
         printf("[1] Interface do Atendente\n");
@@ -35,70 +36,59 @@ int main(){
         scanf("%d%*c", &num);
         sleep(0.1);
 
-        if(num == 1 || num == 2 || num == 0) break;
+        if(num==1){
+            while(1){
+                num = -1;
+                printf("\nBem vinde a sorveteria Tio Bolas!\n\n");
+                printf("O que você deseja fazer?\n");
+                printf("[1] Iniciar um novo pedido\n");
+                printf("[2] Verificar pedidos em andamento\n");
+                printf("[3] Voltar ao menu inicial\n");
+                printf("[0] Terminar programa\n");
+                scanf("%d%*c", &num);
+                sleep(0.1);
+                
+                if(num == 1) inserir_pedido();
+                if(num == 3) break;
+                if(num == 0) return 0;
+                else printf("Opção inválida\n");
+            }
+            if(num == 3) continue;
+            
+        }
+        if(num==2){            
+
+            if(login_gerente() == 0) continue; // Caso o usuário tenha escolhido Voltar ao menu inicial ao logar.
+            while(1){
+                num = -1;
+                printf("\nBem vinde a sorveteria Tio Bolas!\n\n");
+                printf("Acesso liberado, o que deseja fazer?\n");
+                printf("[1] Ver cardápio\n");
+                printf("[2] Ver o estoque\n");
+                printf("[3] Ver finanças\n");
+                printf("[4] Ver clientes\n");
+                printf("[5] Ver historico de pedidos\n");
+                printf("[6] Voltar ao menu inicial\n");
+                printf("[0] Terminar o programa\n");
+                scanf("%d%*c", &num);
+                sleep(0.1);
+
+                if(num == 1) ver_cardapio();
+                else if(num == 6) break;
+                else if(num == 0) return 0;
+                else printf("Opção inválida\n");
+                
+            
+            
+            }
+            if (num == 6) continue;
+
+        }
+        if(num == 0) break;
         else printf("Opção inválida\n");
     }
 
-    if(num==1){
-        while(1){
-            printf("\nBem vinde a sorveteria Tio Bolas!\n\n");
-            printf("O que você deseja fazer?\n");
-            printf("[1] Iniciar um novo pedido\n");
-            printf("[2] Verificar pedidos em andamento\n");
-            printf("[3] Voltar ao menu inicial\n");
-            printf("[0] Terminar programa\n");
-            scanf("%d%*c", &num);
-            sleep(0.1);
-
-            if(num == 1 || num == 2 || num == 0 || num == 3) break;
-            else printf("Opção inválida\n");
-        }
-
-        if(num == 1){
-
-        }
-        else if(num == 2){
-
-        }
-        else if(num == 3){
-            
-        }
-        else if(num == 0){
-            
-        }
-    }
-    if(num==2){
-        while(1){
-            printf("\nBem vinde a sorveteria Tio Bolas!\n\n");
-            printf("Digite a senha (de até 15 caracteres) para ter acesso a interface de gerente:\n");
-            scanf("%s", tentativa);
-
-            for(c=0; c<15; c++) if(senha[c]!=tentativa[c]) break;
-            
-            if(c!=15) printf("Senha invalida, tente novamente\n");
-            else {
-                while(1){
-                    printf("\nBem vinde a sorveteria Tio Bolas!\n\n");
-                    printf("Acesso liberado, o que deseja fazer?\n");
-                    printf("[1] Ver cardápio\n");
-                    printf("[2] Ver o estoque\n");
-                    printf("[3] Ver finanças\n");
-                    printf("[4] Ver clientes\n");
-                    printf("[5] Ver historico de pedidos\n");
-                    printf("[6] Voltar ao menu inicial\n");
-                    printf("[0] Terminar o programa\n");
-                    scanf("%d%*c", &num);
-                    sleep(0.1);
-
-                    if(num == 1) ver_cardapio();
-                    else printf("Opção inválida\n");
-                    printf("aaaaaa\n");
-                }
-            
-            }
-        }
-
-    }
+    
 
 
     return 0;
