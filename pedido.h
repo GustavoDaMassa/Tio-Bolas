ecfd2d6ac6f21a4704ef16a7ce69eb66bcfdbc8d
Anderson Barros Rigonatto <111210996+AndersonRigonatto@users.noreply.github.com>
2023-02-15 14:21:57 -0300

    Update pedido.h

--- pedido.h
+++ pedido.h
@@ -9,7 +9,7 @@ typedef struct pedido Pedido;
 typedef struct head Head;
 
 struct pedido {
-    int index;
+    int qtd_p;
     Produto_ped * produtos;
     double preco_t;
     Cliente * cliente;
@@ -22,8 +22,11 @@ struct head {
 
 extern Head * h;
 
+int pick_name(char * s);
 void inserir_pedido();
 void concluir_pedido();
 void excluir_pedido();
+int escolher_cliente(Cliente * c);
+int escolher_produtos(Pedido * p);
 
 #endif
