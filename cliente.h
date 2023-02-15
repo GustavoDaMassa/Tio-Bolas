af48f586c6ca9b4254ad260d611bdc050cd14eab
Anderson Barros Rigonatto <111210996+AndersonRigonatto@users.noreply.github.com>
2023-02-15 14:19:29 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,8 +1,10 @@
 #ifndef CLIENTE
 #define CLIENTE
 
+
 typedef struct cliente Cliente;
 typedef struct data Data;
+typedef struct clients Clients;
 
 struct data{
     int dia;
@@ -13,17 +15,32 @@ struct data{
 struct cliente{
     int id;
     int cadastro; // 0 quando não tem cadastro, 1 quando tem cadastro.
-    char nome[100];
+    char nome[101];
     Data nasc;
-    char email[100];
+    char email[101];
     int qnt_compras;
 };
+
+struct clients{
+    int qtd;
+    Cliente * clis;
+};
+
+extern Clients * cli;
 
 /* 
 Quando o cliente não tem e não deseja realizar o cadastro, o tipo Cliente ainda será criado e a única 
 identificação será o nome (poderá ser apenas o primeiro), mas ele não será salvo no arquivo clientes.bin
 */
 
-void cadastrar_cliente();
+int cadastrar_cliente();
+int busca_cliente();
+int pick_date(Data * d);
+int pick_email(char * s);
+int define_pos(char * s1);
+int check_cli_name(char * s);
+void save_clientes();
+void load_clientes();
+void print_clientes();
 
 #endif
