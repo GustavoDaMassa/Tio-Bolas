498d32e6f510167833bf7abe4dfbfd3993b7d6d7
Isabela Teixeira e Cerqueira <111237295+IsabelaaTeixeira@users.noreply.github.com>
2023-02-16 11:42:48 -0300

    Delete menu.h

--- menu.h
+++
@@ -1,6 +0,0 @@
-/**
- * @brief Função que lida com o login do gerente. Lê a senha e checa.
- * 
- * @return 0 se o usuário desistiu do login e decidiu voltar ao menu inicial, 1 se o login deu certo
- */
-int login_gerente();
